7695382de46335bf9a694ebaf9b43603e6281ed0
khizerdev <46632721+khizerdev@users.noreply.github.com>
2021-01-01 15:49:24 +0500

    Header Hide Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -74,32 +74,32 @@ function Header() {
 
     return ( !user ? (
 
-        <div className="header">
-            <div className="header__left">
-                    <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt=""/>
-                    <div className="header__search">
-                        <SearchIcon/>
-                        <input type="text"/>
-                    </div>
+       ""
+    ) :  <div className="header">
+    <div className="header__left">
+            <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt=""/>
+            <div className="header__search">
+                <SearchIcon/>
+                <input type="text"/>
             </div>
+    </div>
 
-            <div className="header__right">
-                <HeaderOption Icon={HomeIcon} title="Home"/>
-                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
-                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
-                <HeaderOption Icon={ChatIcon} title="Messaging"/>
-                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
-                <HeaderOption onClick={logoutFromApp} avatar={user?.photoURL} title={user?.displayName}/>
-                {/* <HeaderOption Icon={icon} title={theme} click={toggleTheme}/> */}
-                <div className="headerOption" onClick={toggleTheme}>
-                    { icon }
-                    <h3 className="headerOption__title">
-                        {theme}
-                    </h3>
-                </div>
-            </div>
+    <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home"/>
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+        <HeaderOption Icon={ChatIcon} title="Messaging"/>
+        <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+        <HeaderOption onClick={logoutFromApp} avatar={user?.photoURL} title={user?.displayName}/>
+        {/* <HeaderOption Icon={icon} title={theme} click={toggleTheme}/> */}
+        <div className="headerOption" onClick={toggleTheme}>
+            { icon }
+            <h3 className="headerOption__title">
+                {theme}
+            </h3>
         </div>
-    ) : ""
+    </div>
+</div>
     )
 }
 
